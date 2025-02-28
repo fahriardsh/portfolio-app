ff591998f65b99bde2b5ea14906e7680dfbcf968
Fahri Ardiansyah <fahriardsh@gmail.com>
2025-02-28 16:30:59 +0700

    update portfolio

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -28,6 +28,13 @@ export default function Home() {
         <Project></Project>
         <Journey></Journey>
         {/* <Contact></Contact> */}
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         <footer className="flex h-14 shrink-0 items-center gap-2">
           <div className="flex flex-1 items-center gap-2 px-3">
             <SidebarTrigger />
